bd407c995cea8bd2bb164d87240458412367cbda
masanarih0ri <masanarihoridevelop@gmail.com>
2020-04-25 22:16:08 +0900

    add enum

---
+++ src/enum.ts
@@ -0,0 +1,34 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.January);
+console.log(Months.December);
+
+enum Colors {
+  RED = '#ff0000',
+  WHITE = '#ffffff',
+  BLACK = '#000000',
+}
+
+enum Colors {
+  YELLOW = '#ffff00',
+  GRAY = '#808080',
+}
+
+console.log(Colors.GRAY);
+
+// Colors.YELLOW;
